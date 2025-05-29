a3dd2806eaf4c8259debbe48ce4b5ce8501ca424
itsalongstory <lichao2012@gmail.com>
2025-05-30 00:00:43 +0800

    debug

--- app/models/good_job/i18n_config.rb
+++ app/models/good_job/i18n_config.rb
@@ -3,7 +3,7 @@
 module GoodJob
   class I18nConfig < ::I18n::Config
     BACKEND = I18n::Backend::Simple.new
-    AVAILABLE_LOCALES = GoodJob::Engine.root.join("config/locales").glob("*.yml").map { |path| File.basename(path, ".yml").to_sym }.uniq
+    AVAILABLE_LOCALES = GoodJob::Engine.root.join("config/locales").glob("*.yml").map { |path| File.basename(path, ".yml").to_sym }.uniq + [:'zh-CN']
     AVAILABLE_LOCALES_SET = AVAILABLE_LOCALES.inject(Set.new) { |set, locale| set << locale.to_s << locale.to_sym }
 
     def backend
